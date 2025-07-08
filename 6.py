fc69480aa8560d07d468368e28eee5e6f4d2cffa
Tennill <tennill04g@gmail.com>
2025-07-08 16:36:29 +0530

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,7 @@
+# math module
+import math
+
+number = int(input("Enter a number: "))
+print("Square Root: ",math.sqrt(number))
+print("Logarithm: ",math.log(number))
+print("sine: ",math.sin(number))
